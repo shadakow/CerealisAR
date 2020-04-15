c00018bf5bc41a679b14306af8944d7763180219
Adrien Ceccaldi <adrien.ceccaldi@hotmail.fr>
2020-04-15 11:27:46 +0200

    Add working solution for Unity 2019.3.1
    Android device ready

---
+++ Assets/Cerealis/Scripts/VoxelBustersManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelBusters.NativePlugins;
+using Object = System.Object;
+
+public class VoxelBustersManager : MonoBehaviour
+{
+
+    private bool isSharing = false;
+    
+    public void ShareSocialMedia()
+    {
+        isSharing = true;
+    }
+
+    private void LateUpdate()
+    {
+
+        if (isSharing == true)
+        {
+            isSharing = false;
+
+            StartCoroutine(CaptureScreenShoot());
+        }
+    }
+
+    IEnumerator CaptureScreenShoot()
+    {
+        yield return  new WaitForEndOfFrame();
+
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
+        
+        ShareSheet(texture);
+
+        Destroy(texture);
+    }
+
+    private void ShareSheet(Texture2D texture)
+    {
+        ShareSheet _shareSheet = new ShareSheet();
+        _shareSheet.Text = "#cerealis #coloring #AR";
+        _shareSheet.AttachImage(texture);
+        
+        NPBinding.Sharing.ShowView(_shareSheet, FinishSharing);
+    }
+
+    private void FinishSharing(eShareResult _result)
+    {
+        Debug.Log(_result);
+    }
+}
